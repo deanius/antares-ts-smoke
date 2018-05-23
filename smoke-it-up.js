0ded6cad1dd1d72ee9a33d051e0636be533e79bb
Dean Radcliffe <Dean.Radcliffe@rotary.org>
2018-05-23 14:52:50 -0500

    Adds a speaking-style renderer.
    Thus revealing issues with libraries that call your callback before they are actually done working.

--- smoke-it-up.js
+++ smoke-it-up.js
@@ -1,9 +1,37 @@
 const faker = require("faker")
+const say = require("say")
 
 module.exports = antares => {
   antares.subscribeRenderer(({ action }) => {
     console.log(`Rendering ${action.type}`)
   })
+
+  //  Variable to let us only start speaking once the current one is done - see below!
+  let currentSpeakingIsOver = Promise.resolve()
+
+  antares.subscribeRenderer(
+    ({ action }) => {
+      // naive implementation - multiple speakings may go on at once
+      console.log(`Speaking ${action.payload} ..`)
+      say.speak(action.payload, "Good News", 1.0)
+
+      // full implementation - you promise-chain the current speech
+      // upon the promise that the current speaking is over
+      // note that keeping a queue of renderings is exactly what an epic would be for!
+      // Every sufficiently complex async program contains a bug-riddeen partially impl. RxJS!
+      // let getNextSaying = text => () => {
+      //   return new Promise(resolve => {
+      //     console.log(`Speaking ${text} ..`)
+      //     say.speak(text, "Good News", 1.0, resolve)
+      //   })
+      // }
+
+      // currentSpeakingIsOver = currentSpeakingIsOver.then(
+      //   getNextSaying(action.payload)
+      // )
+    },
+    { mode: "sync" }
+  )
 
   const actions = [
     {
@@ -15,9 +43,16 @@ module.exports = antares => {
       payload: faker.company.catchPhrase()
     }
   ]
-  actions.forEach(action => {
-    antares
-      .process(action)
-      .then(({ action }) => console.log(`Fully processed: ${action.payload}` + "\n"))
-  })
+
+  // you wouldn't normally wait for each promise..
+  actions.reduce((lastActionPromise, action) => {
+    return lastActionPromise.then(() => {
+      return antares
+        .process(action)
+        .then(({ action }) =>
+          console.log(`Fully processed: ${action.payload}` + "\n")
+        )
+    })
+  }, Promise.resolve())
+  .then(() => console.log('Done!'))
 }
